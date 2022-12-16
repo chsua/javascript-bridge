3143621d66643607eccda54d9372ae848c9b9e4a
chsua <csj1919@naver.com>
2022-12-16 15:20:16 +0900

    Style: 오타 수정

--- practice/Constant.js
+++ practice/Constant.js
@@ -16,7 +16,7 @@ const MESSAGE = {
     START: "다리 건너기 게임을 시작합니다.",
     REQUEST_LENGTH: "다리의 길이를 입력해주세요.",
     REQUEST_UPDOWN: "이동할 칸을 선택해주세요. (위: U, 아래: D)",
-    REQUEST_LENGTH: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
+    REQUEST_REGAME: "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)",
     END_FIRST: "최종 게임 결과",
     END_SECOND: "게임 성공 여부: ",
     END_THIRD: "총 시도한 횟수: ",
