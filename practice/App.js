5727bc6a5e7886b9be4ae52576678651e1407577
chsua <csj1919@naver.com>
2022-12-16 16:33:26 +0900

    Feat: 다리 길이 받는 콜백함수 작성

--- practice/App.js
+++ practice/App.js
@@ -1,7 +1,30 @@
+const BridgeGame = require("./BridgeGame") ;
+const InputView = require("./InputView") ;
+const OutputView = require("./OutputView") ;
+
+
+
+
 class App {
-    play() {}
+  play() {
+    OutputView.start() ;
+
   }
+
+  bridgeSizeCallback(size){
+    const game = new BridgeGame(size) ;
+    InputView.readMoving(this.moveCallback) ;
+  }
+
+  moveCallback(command){
+  }
+
+  regameCallback(){
+
+  }
+
+}
   
-  module.exports = App;
+module.exports = App;
   
 
