a19f7f003f4fc91ac14f91ac624e10f24c3cc3f7
chsua <csj1919@naver.com>
2022-12-16 15:43:13 +0900

    Test: 다리만들기 test

--- practice/Game.test.js
+++ practice/Game.test.js
@@ -1,4 +1,5 @@
 const InputView = require("./InputView") ;
+const BridgeMaker = require("./BridgeMaker") ;
 
 describe("Input_test", () => {
 
@@ -31,5 +32,19 @@ describe("Input_test", () => {
 
 })
 
+describe("BridgeMaker", () => {
+    test("다리생성 기능확인", () => {
+        const returnOne = () => {
+            return 1
+        }
+        expect(BridgeMaker.makeBridge(3,returnOne)).toEqual(["U","U","U"])
+    })
 
+    test("다리생성 기능확인_2", () => {
+        const returnZero = () => {
+            return 0
+        }
+        expect(BridgeMaker.makeBridge(4,returnZero)).toEqual(["D","D","D","D"])
+    })
 
+})
