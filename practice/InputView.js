5c57023aa904dc7e1ffa9c1ce0b9151db2d54839
chsua <csj1919@naver.com>
2022-12-16 15:02:56 +0900

    Feat: 다리 길이 유효성 검사

--- practice/InputView.js
+++ practice/InputView.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const { BRIDGE, COMMAND, MESSAGE, ERROR } = require("./Constant");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -6,17 +7,48 @@ const MissionUtils = require("@woowacourse/mission-utils");
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    try {
+      
+    } catch (error) {
+
+    }
+  },
+
+  bridgeSizeValigate(size){
+    if ( !Number.isInteger(size) ) throw new Error (ERROR.ERROR_BRIDGE_LENGTH) ;
+    if ( size < BRIDGE.LENGTH_MIN || size > BRIDGE.LENGTH_MAX ) throw new Error (ERROR.ERROR_BRIDGE_LENGTH) ;
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    try {
+
+    } catch (error) {
+
+    }
+  },
+
+  movingValidate(move){
+    
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand(callback) {
+    try {
+
+    } catch (error) {
+
+    }
+  }, 
+
+  gameCommandValidate(command){
+
+  },
 };
 
 module.exports = InputView;
