02d30b0fc16092ee754acf6755cf84547682acc9
chsua <csj1919@naver.com>
2022-12-16 15:19:38 +0900

    Test: 이동 입력 유효성 test

--- practice/Game.test.js
+++ practice/Game.test.js
@@ -2,15 +2,22 @@ const InputView = require("./InputView") ;
 
 describe("Input_test", () => {
 
-    test("bridgeSize_범위 밖", () => {
+    test("bridgeSize_범위 밖일때 오류", () => {
         expect( () => {
             InputView.bridgeSizeValidate(1) ;
         }).toThrow("[ERROR]")
     })
 
-    test("bridgeSize_글자", () => {
+    test("bridgeSize_글자일때 오류", () => {
         expect( () => {
             InputView.bridgeSizeValidate("a") ;
+        }).toThrow("[ERROR]")
+
+    })
+
+    test("moving_예외처리", () => {
+        expect( () => {
+            InputView.movingValidate("a") ;
         }).toThrow("[ERROR]")
 
     })
